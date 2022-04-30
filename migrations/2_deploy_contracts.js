3c7069c548d1501638cb1ebcc5129b387bc60f2c
mrecho-dev <ekin.yildizhan@hotmail.com>
2022-04-30 17:45:02 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,5 @@
-// var Voting = artifacts.require("./Voting.sol");
-// var VoteAsset = artifacts.require("./VoteAsset.sol");
-
-// module.exports = function(deployer) {
-//   deployer.deploy(Voting);
-//   deployer.deploy(VoteAsset);
-// };
-
-
 const Voting = artifacts.require("./Voting.sol");
 const VoteAsset = artifacts.require("./VoteAsset.sol");
-
-// module.exports = async function(deployer) 
-// {
-// 	// deploy Voting and store return value
-// 	let vote = await deployer.deploy(Voting);
-//   	// deploy the VoteAsset and pass vote
-// 	deployer.deploy(VoteAsset, vote);
-// }
 
 module.exports = function(deployer) 
 {
